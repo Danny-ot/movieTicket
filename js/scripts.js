d40e14c400eb4a46b09791310b18340c91b4bdbf
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-09 02:27:40 -0800

    Create the method for getting price

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Tickets(){
     };
     this.movies = {
         "SpiderMan" : 3000,
-        "Jungle Cruise" : 2500,
+        "Jungle Cruise" : 3000,
         "Lion King" : 3000,
         "Red Notice" : 3000,
         "Doctor Strange" : 3000
@@ -20,16 +20,33 @@ Tickets.prototype.assignId = function(){
     return this.id ;
 }
 
+// Method For Adding Customer
 Tickets.prototype.addCustomer = function(customer){
     customer.id = this.assignId();
     this.customers[customer.id] = customer;
 };
 
+// Method For Getting Pricing
+Tickets.prototype.getPrice = function(id){
+    let customer = this.customers[id];
+    let price = this.movies[customer.movie];
+    if(customer.age > 65){
+        price -= 500;
+    }
+    if(this.id < 3){
+        price -= 300 
+    }
+    if(customer.time ==="12:30" || customer.time === "3:30"){
+        price -= 200
+    }
+    
+    return price;
+}
+
 // Constructor For The Customer
-function Customer(name , age , release , time , movie){
+function Customer(name , age , time , movie){
     this.name = name;
     this.age = age;
-    this.release = release;
     this.time = time;
     this.movie = movie;
 };
