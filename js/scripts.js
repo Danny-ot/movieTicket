3fd927a901866e7cc76eacb284f12243587b031e
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-09 00:42:40 -0800

    Create the constructor for the tickets

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Constructor For The Tickets
+function Tickets(){
+    this.customers = {
+
+    }
+    this.movies = {
+        "SpiderMan" : 3000,
+        "Jungle Cruise" : 2500,
+        "Lion King" : 3000,
+        "Red Notice" : 3000,
+        "Doctor Strange" : 3000
+    }
+}
+
